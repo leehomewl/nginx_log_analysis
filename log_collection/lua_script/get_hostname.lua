8fd66be66ccbbcaaf6491138b03a66fa8663deff
leehome <515947130@qq.com>
2018-12-13 18:05:47 +0800

    Update get_hostname.lua

--- log_collection/lua_script/get_hostname.lua
+++ log_collection/lua_script/get_hostname.lua
@@ -1,6 +1,6 @@
+--Get the hostname of the server
 local utils = require "utils"
 local lua_data = ngx.shared.lua_data;
 local hostname = utils.hostname()
 lua_data:set("hostname", hostname)
---ngx.log(ngx.ERR,'The hostname is  :' ,lua_data:get("hostname"))
 
