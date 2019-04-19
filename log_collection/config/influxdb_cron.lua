d166f93fe233e77fe17ecde8c54ab35baacafec8
leehome <515947130@qq.com>
2019-04-19 16:48:30 +0800

    Update influxdb_cron.lua

--- log_collection/config/influxdb_cron.lua
+++ log_collection/config/influxdb_cron.lua
@@ -8,12 +8,12 @@ local influxdb_table = init_config.influxdb_table
 
 
 _M.continuous_query = {
-    string.format([[CREATE CONTINUOUS QUERY "tp99" ON %s BEGIN  SELECT PERCENTILE(upstream_time,99) as tptime  INTO %s."cq_data_expires".tp99_nginx FROM %s group by  time(1m),url  END]],database,database,influxdb_table),
-    string.format([[CREATE CONTINUOUS QUERY "tp90" ON %s BEGIN  SELECT PERCENTILE(upstream_time,90) as tptime  INTO %s."cq_data_expires".tp90_nginx FROM %s group by  time(1m),url  END]],database,database,influxdb_table),
-    string.format([[CREATE CONTINUOUS QUERY "tp85" ON %s BEGIN  SELECT PERCENTILE(upstream_time,85) as tptime  INTO %s."cq_data_expires".tp85_nginx FROM %s group by  time(1m),url  END]],database,database,influxdb_table),
-    string.format([[CREATE CONTINUOUS QUERY "ave" ON %s BEGIN  select mean(upstream_time) as ave_mean INTO %s."cq_data_expires".ave_nginx FROM %s group by  time(2m),url END]],database,database,influxdb_table),
-    string.format([[CREATE CONTINUOUS QUERY "pv" ON %s BEGIN  select count(uri) as pv_count INTO %s."cq_data_expires".pv_nginx FROM %s group by  time(1m),url END]],database,database,influxdb_table),
-    string.format([[CREATE CONTINUOUS QUERY "uri_status_group" ON %s BEGIN  select count(uri) as status_count INTO %s."cq_data_expires".usg_nginx FROM %s  group by  time(1m),url,status  END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "tp99" ON %s BEGIN  SELECT PERCENTILE(upstream_time,99) as tptime  INTO %s."cq_data_expires".tp99_nginx FROM %s group by  time(1m),mysql_host_uri  END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "tp90" ON %s BEGIN  SELECT PERCENTILE(upstream_time,90) as tptime  INTO %s."cq_data_expires".tp90_nginx FROM %s group by  time(1m),mysql_host_uri  END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "tp85" ON %s BEGIN  SELECT PERCENTILE(upstream_time,85) as tptime  INTO %s."cq_data_expires".tp85_nginx FROM %s group by  time(1m),mysql_host_uri  END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "ave" ON %s BEGIN  select mean(upstream_time) as ave_mean INTO %s."cq_data_expires".ave_nginx FROM %s group by  time(2m),mysql_host_uri END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "pv" ON %s BEGIN  select count(uri) as pv_count INTO %s."cq_data_expires".pv_nginx FROM %s group by  time(1m),mysql_host_uri END]],database,database,influxdb_table),
+    string.format([[CREATE CONTINUOUS QUERY "uri_status_group" ON %s BEGIN  select count(uri) as status_count INTO %s."cq_data_expires".usg_nginx FROM %s  group by  time(1m),mysql_host_uri,status  END]],database,database,influxdb_table),
 }
 
 
